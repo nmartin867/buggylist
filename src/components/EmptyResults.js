7a3b542a56f73320e6f483a66c66d098302fcfb4
nmartin867 <nmartin867@gmail.com>
2017-11-25 21:59:11 -0700

    Ignore IDE stuff

--- src/components/EmptyResults.js
+++ src/components/EmptyResults.js
@@ -1,1 +1,13 @@
 
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const EmptyResults = ({ onLoadClick }) => {
+
+}
+
+EmptyResults.propTypes = {
+    onLoadClick: PropTypes.func.isRequired
+}
+
+export default EmptyResults
