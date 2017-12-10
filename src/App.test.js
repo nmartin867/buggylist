a1da4a8d8ce0ae0a3b0b9652b8aee5c6a56fa3a6
nmartin867 <nmartin867@gmail.com>
2017-12-09 18:47:24 -0700

    Fix Jest Test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import {Provider} from 'react-redux'
 import ReactDOM from 'react-dom';
 import App from './App';
+import configureStore from './store/configureStore'
+
+let store = configureStore({
+    isLoading: false,
+    hasError: false,
+    images: []
+})
+
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+    const div = document.createElement('div');
+    ReactDOM.render(
+        <Provider store={store}>
+            <App/>
+        </Provider>, div);
 });
